ef2d90e6de4769f75b2fe736aedfae9f7d5335ae
Craig Laukiavich <claukiavich@duramaxmarine.com>
2021-07-13 14:28:14 -0400

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+	}
+}
